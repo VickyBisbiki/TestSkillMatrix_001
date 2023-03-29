008ff60cf5db5c8a47202db7caa731a918ef36dc
VickyBisbiki <vicksbiz@gmail.com>
2023-03-29 15:51:29 +0300

    connection string

---
+++ TestSkillMatrix_001/Migrations/20230329123839_DepaertmentFixed.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TestSkillMatrix_001.Migrations
+{
+    /// <inheritdoc />
+    public partial class DepaertmentFixed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
