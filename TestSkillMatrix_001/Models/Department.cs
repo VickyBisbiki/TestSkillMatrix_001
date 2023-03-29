114ce6542e3de9b4969145c64fd78c758eb9ee6b
VickyBisbiki <vicksbiz@gmail.com>
2023-03-29 15:37:08 +0300

    Department fixed

--- TestSkillMatrix_001/Models/Department.cs
+++ TestSkillMatrix_001/Models/Department.cs
@@ -5,7 +5,8 @@
 
         public int Id { get; set; }
         public string? Name { get; set; }
-        public List<Supervisor>? Supervisors { get; set; }
+        public List<Supervisor> Supervisors { get; set; }
+
         public Department(string name)
         {
             Name = name;
